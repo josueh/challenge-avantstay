08b3f9fcde749a7ad329f20a287ad092d42d8fc1
Josué Machado <josueh.developer@gmail.com>
2023-08-25 17:49:59 -0300

    Test query params using `InputSearchText` demo component

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,4 +3,15 @@ describe('Home', () => {
     cy.visit('/')
     cy.contains(/avantstay/i).should('be.visible')
   })
+
+  it('should load and render query string `query` when defined', () => {
+    cy.visit('/?query=california')
+    cy.contains(/california/i).should('be.visible')
+  })
+
+  it('should update query strings `query` when search input changes', () => {
+    cy.visit('/')
+    cy.get('input[name=search-text]').type('killing bugs')
+    cy.url().should('match', /killing.*bugs/i)
+  })
 })
