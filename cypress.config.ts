575142c6b5286dff2a6ae9c5fd8e191e0a0d278c
Josué Machado <josueh.developer@gmail.com>
2023-09-11 13:27:14 -0300

    Update `actions/upload-artifact` to v3 and set up secrets

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,5 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    defaultCommandTimeout: 15000,
   },
-  screenshotOnRunFailure: true,
-  video: true,
 })
