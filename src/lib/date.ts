457350983d92a73baa75323c56e22b25143c7d7e
Josué Machado <josueh.developer@gmail.com>
2023-09-17 02:43:07 -0300

    Add helper `formatRangePeriod` function

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -9,3 +9,17 @@ export function convertToSQLDate(value: Date) {
   const date = dayjs(value)
   return date.format('YYYY-MM-DD')
 }
+
+export function formatRangePeriod(start: Date | string, end: Date | string) {
+  const dateA = dayjs(start)
+  const dateB = dayjs(end)
+  const sameMonth = dateA.month() === dateB.month()
+  const sameYear = dateA.year() === dateB.year()
+  if (sameYear && sameMonth) {
+    return dateA.format('MMM DD') + ' - ' + dateB.format('DD, YYYY')
+  }
+  if (sameYear) {
+    return dateA.format('MMM DD') + ' - ' + dateB.format('MMM DD, YYYY')
+  }
+  return dateA.format('MMM DD, YYYY') + ' - ' + dateB.format('MMM DD, YYYY')
+}
